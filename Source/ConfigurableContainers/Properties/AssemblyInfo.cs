d74e78dfb532c8b69962a202c331bdd51e765e8b
Lisias T <me@lisias.net>
2018-10-27 11:42:14 -0200

    Versioning Normalizing

--- Source/ConfigurableContainers/Properties/AssemblyInfo.cs
+++ Source/ConfigurableContainers/Properties/AssemblyInfo.cs
@@ -24,12 +24,13 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-#if NIGHTBUILD
-[assembly: AssemblyVersion("2.4.*")]
-#else
-[assembly: AssemblyVersion("2.4.3.4")]
-#endif
-[assembly: KSPAssembly("ConfigurableContainers", 2, 4)]
+[assembly: AssemblyVersion(ConfigurableContainers.Version.Number)]
+[assembly: AssemblyFileVersion(ConfigurableContainers.Version.Number)]
+[assembly: KSPAssembly("ConfigurableContainers", ConfigurableContainers.Version.major, ConfigurableContainers.Version.minor)]
+
+//[assembly: KSPAssemblyDependency("KSPe", 2, 0)]
+[assembly: KSPAssemblyDependency("AT_Utils", 1, 6)]
+
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
@@ -37,17 +38,17 @@ using System.Reflection;
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
-namespace AT_Utils
-{
-    public class CCModInfo : KSP_AVC_Info
-    {
-        public CCModInfo()
-        {
-            MinKSPVersion = new Version(1,4,5);
-            MaxKSPVersion = new Version(1,4,5);
+//namespace AT_Utils
+//{
+//    public class CCModInfo : KSP_AVC_Info
+//    {
+//        public CCModInfo()
+//        {
+//            MinKSPVersion = new Version(1,4,5);
+//            MaxKSPVersion = new Version(1,4,5);
 
-            VersionURL   = "https://github.com/allista/ConfigurableContainers/tree/master/GameData/ConfigurableContainers/ConfigurableContainers.version";
-            UpgradeURL   = "https://spacedock.info/mod/1002/Configurable%20Containers";
-        }
-    }
-}
+//            VersionURL   = "https://github.com/allista/ConfigurableContainers/tree/master/GameData/ConfigurableContainers/ConfigurableContainers.version";
+//            UpgradeURL   = "https://spacedock.info/mod/1002/Configurable%20Containers";
+//        }
+//    }
+//}
